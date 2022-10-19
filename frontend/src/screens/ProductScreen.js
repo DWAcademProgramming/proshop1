04d2d01b24163eef54b2531d2d44f742c10271c7
gntpldes <h3c6j6@u.northwestern.edu>
2022-10-19 15:59:17 -0500

    I think this should work

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,28 @@
-import React from 'react'
-
-const ProductScreen = () => {
+import React from "react"
+import { Link, useParams } from "react-router-dom"
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
+import Rating from "../components/Rating"
+import products from "../products"
+ 
+const ProductScreen = ({ match }) => {
+  console.log(products)
+  const { id } = useParams()
+  console.log(id)
+  const product = products.find((p) => p._id === id)
+ 
   return (
-    <div>
-        Product 
-        <h2>Remove this</h2>
-    </div>
+    <>
+      <Link className="btn btn-dark my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}></Col>
+      </Row>
+    </>
   )
 }
-
+ 
 export default ProductScreen
