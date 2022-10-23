ce8ef17ccd4537aec189219168ae94f349fa78f4
gntpldes <h3c6j6@u.northwestern.edu>
2022-10-23 17:23:49 -0500

    Errors fixed

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,22 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import { Link, useParams } from "react-router-dom"
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
 import Rating from "../components/Rating"; 
 import products from "../product"
+import axios from 'axios'
  
 const ProductScreen = ({ match }) => {
-  console.log(products)
-  const { id } = useParams()
-  console.log(id)
-  const product = products.find((p) => p._id === id)
+  const [product, setProduct] = useState([]);
+
+  useEffect(() =>{
+    const fetchProduct = async () =>{
+      const {data} = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [match])
  
   return (
     <>
