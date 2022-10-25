499bedce19a0d948d0cca9e155928b3702afdd14
David <95488893+DWAcademProgramming@users.noreply.github.com>
2022-10-25 17:23:46 -0500

    Hour 13 sucks...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import { Container, Row, Col } from "react-bootstrap"
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
+import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
+import ProfileScreen from "./screens/ProfileScreen"
  
 const App = () => {
   return (
@@ -15,10 +18,13 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Routes>
-            <Route path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
             <Route path="/cart/" element={<CartScreen />} />
+            <Route path="/" element={<HomeScreen />} />
           </Routes>
         </Container>
       </main>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import {LinkContainer} from 'react-router-bootstrap'
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { logout } from '../actions/userAction'
  
 function Header() {
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector(state => state.userLogin)
+  const userInfo = userLogin
+
+  const logoutHandler = () =>{
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect fixed='top'>
@@ -16,9 +28,19 @@ function Header() {
               <Nav.Link as={Link} to='/cart'>
                 <i className='fas fa-shopping-cart'></i> Cart
               </Nav.Link>
-              <Nav.Link as={Link} to='/login'>
-                <i className='fa fa-user'></i> Sign In
-              </Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ): <Nav.Link as={Link} to='/login'>
+              <i className='fa fa-user'></i> Sign In
+            </Nav.Link>}
+              
             </Nav>
           </Navbar.Collapse>
         </Container>
